6b1b8e52c8bcfc746621b6540c889dc27cd490c6
synbyte <rashnikov2.0@gmail.com>
2021-09-26 02:37:33 -0700

    1 file modified

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueMaterial from 'vue-material';
-
-Vue.use(VueMaterial);
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+ 
+Vue.use(VueMaterial)
 
 new Vue({
   el: '#app',
